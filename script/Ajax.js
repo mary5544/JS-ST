0d6e3b1c20193410143a75470786e0e1bca2d4f5
김성민 <mary5544@gimseongmin-ui-MacBookPro.local>
2022-07-10 16:58:19 +0900

    .

--- script/Ajax.js
+++ script/Ajax.js
@@ -1,12 +1,12 @@
 if(location.pathname === '/data/sub_01.html'){
   (function StudyList_01(){
-    fetch('../data/함수/LIST').then(function(response){
+    fetch('./data/함수/LIST').then(function(response){
       response.text().then(function(text){
         let ArrList = text.split(',');
         let tags = '';
         for(Arr in ArrList){
           let list = ArrList[Arr].trim();
-          let tag = `<a class='List' href="../data/함수/${list}.html">${list}.html</a>`;
+          let tag = `<a class='List' href="./data/함수/${list}.html">${list}.html</a>`;
           tags += tag;
         };
         document.getElementById('StudyList').innerHTML = tags;
@@ -17,13 +17,13 @@ if(location.pathname === '/data/sub_01.html'){
 
 if(location.pathname === '/data/sub_02.html'){
   (function StudyList_02(){
-    fetch('../data/객체/LIST').then(function(response){
+    fetch('./data/객체/LIST').then(function(response){
       response.text().then(function(text){
         let ArrList = text.split(',');
         let tags = '';
         for(Arr in ArrList){
           let list = ArrList[Arr].trim();
-          let tag = `<a class='List' href="../data/객체/${list}.html">${list}.html</a>`;
+          let tag = `<a class='List' href="./data/객체/${list}.html">${list}.html</a>`;
           tags += tag;
         };
         document.getElementById('StudyList').innerHTML = tags;
